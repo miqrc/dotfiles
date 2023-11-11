3518be3aa935553ac01a0382027b09db85ee95d1
Miquel Ruiz <miqrc@pm.me>
2023-11-11 19:01:08 +0100

    nvim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -9,6 +9,7 @@ vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap =
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>gg', '<cmd>Neogit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeToggle<CR>', {noremap = true, silent = true})
 
 -- Set options
 vim.opt.relativenumber = true
@@ -18,6 +19,17 @@ vim.opt.expandtab = true   -- Use spaces instead of tabs
 vim.opt.softtabstop = 4    -- Number of spaces tabs count for in insert mode
 vim.opt.clipboard = "unnamedplus"
 
+-- disable netrw 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.cmd [[
+" NERDTree settings go here
+let NERDTreeShowHidden=1
+let g:NERDTreeQuitOnOpen = 1
+]]
+
+
+
 -- L&F
 local status_ok, gruvbox = pcall(require, "gruvbox")
 if status_ok then
@@ -25,49 +37,6 @@ if status_ok then
     vim.cmd([[colorscheme gruvbox]])
 end
 
--- NvimTree
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
--- setup 
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if status_ok then
-    nvim_tree.setup({
-        on_attach = function(client, bufnr)
-            local api = require "nvim-tree.api"
-
-            local function opts(desc)
-                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-            end
-
-            -- default mappings
-            api.config.mappings.default_on_attach(bufnr)
-
-            -- custom mappings
-            vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
-            vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
-        end,
-        sort_by = "case_sensitive",
-        view = {
-            width = 30,
-        },
-        renderer = {
-            group_empty = true,
-        },
-        filters = {
-            dotfiles = true,
-        },
-        actions = {
-            open_file = {
-                quit_on_open = true,
-            },
-        },
-    })
-end
 
 
 local status_ok, lualine = pcall(require, "lualine")
@@ -142,12 +111,12 @@ end
 local status_ok, neogit = pcall(require, "neogit")
 if status_ok then
     neogit.setup({
-      -- Add your configuration here, or leave it empty to use the default settings
-      -- For example:
-      integrations = {
-        -- If you have 'nvim-cmp' installed, you can enable this setting
-        diffview = true  
-      },
+        -- Add your configuration here, or leave it empty to use the default settings
+        -- For example:
+        integrations = {
+            -- If you have 'nvim-cmp' installed, you can enable this setting
+            diffview = true  
+        },
     })
 end
 
@@ -291,12 +260,7 @@ return require('packer').startup(function(use)
 
     use { "ellisonleao/gruvbox.nvim" }
 
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional
-        },
-    }
+    use 'preservim/nerdtree'
 
     use {
         'nvim-lualine/lualine.nvim',
